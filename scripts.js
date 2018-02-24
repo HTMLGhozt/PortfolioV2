8d1ecdea1eb84b3a5a1e0e28aaee59fc21a2a8ae
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-02-24 00:28:29 -0700

    end of night

--- scripts.js
+++ scripts.js
@@ -3,12 +3,9 @@ let activeSection = document.querySelector('.home');
 const clickFunction = (e) => {
   e.preventDefault();
   activeSection.classList.remove('active');
-  console.log(e.target.innerText)
   activeSection = document.querySelector(`.${e.target.innerText.toLowerCase()}`);
-  console.log(activeSection)
   activeSection.classList.add('active')
 };
-console.log(links);
 links.forEach(link => {
   link.onclick = clickFunction;
 });
